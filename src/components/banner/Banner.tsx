5c255ac005fd7a504373b20a7ae06250ca25f14d
eitu-university <eitu_fl@hotmail.com>
2025-09-22 11:03:22 -0500

    ajuste banner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,6 +3,7 @@ import styles from './Banner.module.scss';
 
 import { useEffect, useState } from 'react';
 import { FiArrowDown } from 'react-icons/fi';
+import { SvgWave } from './SvgWave';
 
 export default function Banner() {
   const [scrollY, setScrollY] = useState(0);
@@ -35,11 +36,6 @@ export default function Banner() {
         {/* Background Image */}
         <div
           className={`absolute inset-0 transform bg-cover bg-center duration-1000 ease-in-out ${scrollY > 50 ? 'scale-110' : ''} bg-no-repeat ${imgUrlMobile} ${imgUrlDesktop}`}
-          style={
-            {
-              // transform: `translateY(${scrollY * 0.5}px)`,
-            }
-          }
         >
           {/* Overlay */}
           <div className="absolute inset-0 bg-black/50 dark:bg-black/70" />
@@ -97,20 +93,3 @@ export default function Banner() {
   );
 }
 
-const SvgWave = () => (
-  <div className="absolute bottom-0 left-0 right-0 mb-[-2px]">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1440 320"
-      className="h-auto w-full"
-      preserveAspectRatio="none"
-    >
-      <path
-        fill="currentColor"
-        fillOpacity="1"
-        className="text-gray-50 dark:text-gray-900"
-        d="M0,192L80,213.3C160,235,320,277,480,293.3C640,309,800,299,960,298.7C1120,299,1280,309,1360,314.7L1440,320L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-      />
-    </svg>
-  </div>
-);
