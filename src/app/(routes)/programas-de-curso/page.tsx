3876273a4349942b21d4dd596234dfc0579c070a
eitu-university <eitu_fl@hotmail.com>
2025-09-22 17:36:14 -0500

    adding programs pages, banner and animations

--- src/app/(routes)/programas-de-curso/page.tsx
+++ src/app/(routes)/programas-de-curso/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { SvgWave } from '@/components/banner/SvgWave';
+import styles from './ProgramasDeCurso.module.scss';
 
 const ProgramasDeCurso = () => {
   return (
-    <div>ProgramasDeCurso</div>
-  )
-}
+    <div className="relative">
+      <section id="home" className={`${styles.banner}`}>
+        {/* CONTENIDO */}
+        <div className="container relative z-10 mx-auto flex justify-center px-4">
+          <div className={styles.containerAnimate}>
+            <h1 className="mb-6 flex flex-col items-center justify-center text-5xl font-bold leading-tight text-white md:text-7xl">
+              Programas de Curso
+            </h1>
 
-export default ProgramasDeCurso
+            <p className="text-small mb-8 leading-relaxed text-gray-200 md:text-xl">
+              Más que conocimiento, una vida dedicada al servicio.
+            </p>
+          </div>
+        </div>
+        <SvgWave />
+      </section>
+    </div>
+  );
+};
+
+export default ProgramasDeCurso;
--- src/app/(routes)/programas-de-estudio/page.tsx
+++ src/app/(routes)/programas-de-estudio/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { SvgWave } from '@/components/banner/SvgWave';
+import styles from './ProgramasDeEstudio.module.scss';
 
 const ProgramasDeEstudio = () => {
   return (
-    <div>ProgramasDeEstudio</div>
-  )
-}
+    <div className="relative">
+      <section id="home" className={`${styles.banner}`}>
+        {/* CONTENIDO */}
+        <div className="container relative z-10 mx-auto flex justify-center px-4">
+          <div className={styles.containerAnimate}>
+            <h1 className="mb-6 flex flex-col items-center justify-center text-5xl font-bold leading-tight text-white md:text-7xl">
+              Programas de Estudio
+            </h1>
 
-export default ProgramasDeEstudio
+            <p className="text-small mb-8 leading-relaxed text-gray-200 md:text-xl">
+              Equipando a los líderes de hoy para la misión de mañana.
+            </p>
+          </div>
+        </div>
+        <SvgWave />
+      </section>
+    </div>
+  );
+};
+
+export default ProgramasDeEstudio;
